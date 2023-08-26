332be22f40164b9418397a5cb54c20cdfc6fca17
MorentDennis <dennis.morent@student.howest.be>
2023-08-26 11:53:31 +0200

    fixes date

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -3,7 +3,7 @@ import { createClient, type ClientConfig } from "@sanity/client";
 const config: ClientConfig = {
   projectId: "wv7us4c0",
   dataset: "production",
-  apiVersion: "2023-08-28",
+  apiVersion: "2023-08-29",
   useCdn: true,
 };
 
