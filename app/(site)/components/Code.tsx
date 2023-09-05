9e2c3d619fa671ec68f20eade1049b7d0d4f456a
MorentDennis <dennis.morent@student.howest.be>
2023-09-05 20:09:50 +0200

    use refractor for highlighting to use ssr

--- app/(site)/components/Code.tsx
+++ app/(site)/components/Code.tsx
@@ -4,6 +4,18 @@ import React from 'react';
 import SyntaxHighlighter  from 'react-syntax-highlighter';
 import { nightOwl } from 'react-syntax-highlighter/dist/esm/styles/hljs'; // Import the appropriate style
 
+// RichTextComponents.tsx
+import Refractor from "react-refractor";
+// Load any languages you want to use from `refractor`
+import js from "refractor/lang/javascript";
+import typescript from "refractor/lang/typescript";
+import tsx from "refractor/lang/tsx";
+
+//You'll need to register the languages you want to use 
+Refractor.registerLanguage(js);
+Refractor.registerLanguage(typescript);
+Refractor.registerLanguage(tsx);
+
 interface CodeProps {
   value: {
     language: string;
@@ -11,18 +23,15 @@ interface CodeProps {
   }
 }
 
-export const codeComponent: React.FC<CodeProps>= ({ value} ) => { 
-  return (<SyntaxHighlighter 
-     language={value.language} style={nightOwl}> 
-    {value.code}
-  </SyntaxHighlighter>)}
 
-
-
-
-export const customBlockComponents = {
+export const CustomComponents = {
   types: {
-    code: codeComponent
-  }
+    code: ({ value }: CodeProps) => {
+        return (
+          <Refractor language={value.language} value={value.code} />
+        );
+    },
+  },
 }
+
 
--- app/(site)/posts/[post]/page.tsx
+++ app/(site)/posts/[post]/page.tsx
@@ -1,27 +1,40 @@
-'use client'
 import Image from "next/image";
 import { getSinglePost } from "@/sanity/sanity.query";
 import type { PostType } from "@/types";
 import { PortableText } from "@portabletext/react";
 import fallBackImage from "@/public/project.png";
-import { customBlockComponents } from "../../components/Code";
+import { CustomComponents } from "../../components/Code";
 import { use } from "react";
 import Head from "next/head";
+import { Metadata } from "next";
 type Props = {
   params: {
     post: string;
   };
 };
 
-export default function Project({ params }: Props) {
+
+// Dynamic metadata for SEO
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const slug = params.post;
-  const post: PostType = use(getSinglePost(slug))
+  const post: PostType = await getSinglePost(slug)
+
+  return {
+    title: `${post.name} | Project`,
+    description: post.name,
+    openGraph: {
+      images: post.mainImage?.image || "add-a-fallback-project-image-here",
+      title: post.name,
+      description: post.name,
+    },
+  };
+}
+
+export default async function Project({ params }: Props) {
+  const slug = params.post;
+  const post: PostType = await getSinglePost(slug)
 
   return (<>
-    <Head>
-    <title>{post.title}</title>
-    <meta name='description' content='I hope this tutorial is helpful for you' />
-    </Head>
     <main className="max-w-6xl mx-auto lg:px-16 px-8">
       <div className="max-w-3xl mx-auto">
         <div className="flex items-start justify-between mb-4">
@@ -39,7 +52,7 @@ export default function Project({ params }: Props) {
         />
 
         <div className="flex flex-col gap-y-6 mt-8 leading-7 text-zinc-400">
-        <PortableText value={post.content} components={customBlockComponents}/>
+        <PortableText value={post.content} components={CustomComponents}/>
         </div>
       </div>
     </main>
